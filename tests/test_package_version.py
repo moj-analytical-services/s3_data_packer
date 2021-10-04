9f5bd407b8e1b33989cafd4651949b9b3425c777
s-ducks <stephen.bias@digital.justice.gov.uk>
2021-10-04 12:58:00 +0100

    fix package version test

--- tests/test_package_version.py
+++ tests/test_package_version.py
@@ -1,8 +1,8 @@
-# import toml
-# import s3_data_packer
+import toml
+import s3_data_packer
 
 
-# def test_pyproject_toml_matches_version():
-#     with open("pyproject.toml") as f:
-#         proj = toml.load(f)
-#     assert s3_data_packer.__version__ == proj["tool"]["poetry"]["version"]
+def test_pyproject_toml_matches_version():
+    with open("pyproject.toml") as f:
+        proj = toml.load(f)
+    assert s3_data_packer.__version__ == proj["tool"]["poetry"]["version"]
